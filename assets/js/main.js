d9a22ae2b2033bfd61ccf374b141b0364ae69191
Andrea Marangon <marangonandrea84@gmail.com>
2021-04-12 17:13:15 +0200

    milestone 1 terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,25 @@ var app = new Vue({
     movies: [],
     movieSearch: '',
   },
-  mounted(){
-    axios.get('https://api.themoviedb.org/3/search/movie?api_key=12d57af005908ff69d6d243e223e1493&query=ritorno al futuro&language=it')
-     .then((result) => {
-       this.movies = result.data.results;
-       console.log(this.movies);
-
-      });
-
-  },
+  // mounted(){
+  //   axios.get('https://api.themoviedb.org/3/search/movie?api_key=12d57af005908ff69d6d243e223e1493&query=ritorno al futuro&language=it')
+  //    .then((result) => {
+  //      this.movies = result.data.results;
+  //      console.log(this.movies);
+  //
+  //     });
+  //
+  // },
   methods:{
+    searchMovie: function () {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=12d57af005908ff69d6d243e223e1493&query='+ this.movieSearch +'&language=it-IT')
+       .then((result) => {
+         this.movies = result.data.results;
+         console.log(this.movies);
 
+        });
+
+    }
   }
 
 
