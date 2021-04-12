8e4ca97df478c4ad36856e8030ce03f989ff5de8
Andrea Marangon <marangonandrea84@gmail.com>
2021-04-12 15:45:51 +0200

    layout di base

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+Vue.config.devtools = true;
+var app = new Vue({
+  el: '#root',
+  data: {
+    movies: [],
+    movieSearch: '',
+  },
+  mounted(){
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=12d57af005908ff69d6d243e223e1493&query=ritorno al futuro&language=it')
+     .then((result) => {
+       this.movies = result.data.results;
+       console.log(this.movies);
+
+      });
+
+  },
+  methods:{
+
+  }
+
+
+});
